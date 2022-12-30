acc7115b2e6fa2335093dac36ce8379fde104959
Kosuke Ono <ono.kosuke.16@shizuoka.ac.jp>
2022-12-31 01:57:23 +0900

    add spinbox and its label on the fitting panel

--- MainWindow.py
+++ MainWindow.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5.QtWidgets import (
     QMainWindow, QAction, QFileDialog, QApplication, QMdiArea, QMdiSubWindow, QLabel,
-    QComboBox, QPushButton, QWidget)
+    QComboBox, QPushButton, QWidget, QDoubleSpinBox)
 import pandas as pd
 
 
@@ -56,15 +56,16 @@ class MainWindow(QMainWindow):
         if fPath[0]:
             dataset = pd.read_csv(fPath[0], header = 32)
 
+    #XPSのfittingに際して使用する各種windowの表示を行うメソッド
     def show_XPSpanel(self):
-        self.XPS = XPS_FittingPanel()
+        self.XPS = XPS_FittingPanels()
         self.mdi.addSubWindow(self.XPS.FitPanel)
         self.mdi.addSubWindow(self.XPS.DataPanel)
         self.XPS.FitPanel.show()
         self.XPS.DataPanel.show()
 
-
-class XPS_FittingPanel(QWidget):
+#XPSのfittingに際して使用するwindow類の定義クラス
+class XPS_FittingPanels(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
@@ -73,8 +74,8 @@ class XPS_FittingPanel(QWidget):
         #---------Setting for Fit Panel---------
         self.FitPanel = QMdiSubWindow()
         self.FitPanel.setWindowTitle("Fitting Panel")
-        self.FitPanel.setFixedWidth(800)
-        self.FitPanel.setFixedHeight(300)
+        self.FitPanel.setFixedWidth(950)
+        self.FitPanel.setFixedHeight(350)
 
         #コンボボックスの生成
         self.combo_SpectraName = QComboBox(self.FitPanel)
@@ -83,6 +84,38 @@ class XPS_FittingPanel(QWidget):
         SpectraName = [1, 2, 3]
         for i in SpectraName:
             self.combo_SpectraName.addItem(f"{i}") #ループでリストの中身をコンボボックスの表示値に
+
+        #スピンボックスの生成
+        for i in range(0, 36, 1):
+            self.spinBOX = QDoubleSpinBox(self.FitPanel)
+            if i <= 5:
+                self.spinBOX.move(70+(140*i), 80)
+                self.spinBOX.index = f"B.E. {i+1}"
+            
+            elif 6 <= i <= 11:
+                self.spinBOX.move(70+(140*(i-6)), 110)
+                self.spinBOX.index = f"Int. {i-5}"
+
+            elif 12 <= i <= 17:
+                self.spinBOX.move(70+(140*(i-12)), 140)
+                self.spinBOX.index = f"W_gau. {i-11}"
+
+            elif 18 <= i <= 23:
+                self.spinBOX.move(70+(140*(i-18)), 170)
+                self.spinBOX.index = f"Gamma(wid Lorentzian). {i-17}"
+
+            elif 24 <= i <= 29:
+                self.spinBOX.move(70+(140*(i-24)), 200)
+                self.spinBOX.index = f"S.O.S. {i-23}"
+
+            elif 30 <= i <= 35:
+                self.spinBOX.move(70+(140*(i-30)), 230)
+                self.spinBOX.index = f"B.R. {i-29}"
+
+        ParamName = ['B.E.', 'Int.', 'Wid_G', 'gamma', 'S.O.S.', 'B.R.']
+        for i in range(len(ParamName)):
+            self.Label_Param = QLabel(ParamName[i], self.FitPanel)
+            self.Label_Param.move(20, 30*i+80)
 
         #ボタンの生成
         ButtonName = ['Open Graph', 'Check', 'Fit']
